df11378d6c292428e0e444213ba40d883b6dacb9
Dustyn <dustyn_gibb@hotmail.com>
2023-11-07 05:56:22 -0500

    Changed test to ensure I am able to decrypt messages pulled from the database, it works as expected I am able to pull the binary objects from the database, cast them to ByteArray , save as a .gpg and then decrypt the message on my pc using my private key.

--- src/test/kotlin/com/pgpmessenger/ApplicationTest.kt
+++ src/test/kotlin/com/pgpmessenger/ApplicationTest.kt
@@ -1,19 +1,11 @@
 package com.pgpmessenger
 
-import com.pgpmessenger.configuration.configureRouting
-import com.pgpmessenger.functionality.encryption.encryptMessage
+import com.pgpmessenger.database.getUserMessages
 import com.pgpmessenger.functionality.encryption.saveAsGPGFile
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.client.utils.EmptyContent.status
-import io.ktor.http.*
 import io.ktor.server.testing.*
-import junit.framework.TestCase.assertEquals
+import org.jetbrains.exposed.sql.Database
 import org.junit.Test
-import java.io.BufferedOutputStream
-import java.io.FileOutputStream
-import java.util.*
-import kotlin.test.*
+import kotlin.test.assertNotNull
 
 
 class ApplicationTest {
@@ -21,24 +13,21 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            //this doesnt work because of the way the jwt secret is setup but if you take this out it wont build so just leave it goddamn it
-           val encryptedMsg = encryptMessage("-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
-                    "\n" +
-                    "mDMEZUYgmRYJKwYBBAHaRw8BAQdA5zCOiIy2HhWNs4ndZZNYl27MK+uuURb+7a8A\n" +
-                    "kXCYOr60JUR1c3R5biBHaWJiIDxkdXN0eW5fZ2liYkBob3RtYWlsLmNvbT6ImQQT\n" +
-                    "FgoAQRYhBIPPEfdPx2Enaqv6Poh4QQcJAJDbBQJlRiCZAhsDBQkFpUP3BQsJCAcC\n" +
-                    "AiICBhUKCQgLAgQWAgMBAh4HAheAAAoJEIh4QQcJAJDbPb0BAJwneb1nvldgzpHC\n" +
-                    "jJNpJz/kYG7CuWVvbzCvputkFAwDAP9vgADu63jysrji8TygyOxfVIWZdPjwihO5\n" +
-                    "ePjS0dzMD7g4BGVGIJkSCisGAQQBl1UBBQEBB0BEuw9nLerdRqI5eLRDslP2hn2t\n" +
-                    "pVgvWdFhJ9V/AYCsZgMBCAeIfgQYFgoAJhYhBIPPEfdPx2Enaqv6Poh4QQcJAJDb\n" +
-                    "BQJlRiCZAhsMBQkFpUP3AAoJEIh4QQcJAJDbgfoBAIS2cb6J2NUCxSk5i1Dhkqff\n" +
-                    "uDlNzTIhee1BMs51X2B3AQDd4SGvKyn5HznZRBlrFpTPVglcMf15NbAHCFdPbOA/\n" +
-                    "BQ==\n" +
-                    "=LRPh\n" +
-                    "-----END PGP PUBLIC KEY BLOCK-----\n","test hello dustyn")
-            saveAsGPGFile(encryptedMsg,"testfile")
-            assertNotNull(encryptedMsg)
-            println(encryptedMsg)
+            val url = System.getenv("POSTGRES_URL")
+            val user = System.getenv("POSTGRES_USER")
+            val password = System.getenv("POSTGRES_PASSWORD")
+
+            try {
+                Database.connect(url, driver = "org.postgresql.Driver", user = user, password = password)
+
+            } catch (e: Exception) {
+                println(e)
+            }
+            val messages = getUserMessages(1)
+            saveAsGPGFile(messages[0].encryptedMessage, "testing")
+            assertNotNull(messages)
+            println(messages)
+
         }
     }
 }
