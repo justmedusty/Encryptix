664432a94e08aa25334892ff8b991bc2ba8fb020
Dustyn <dustyn_gibb@hotmail.com>
2023-11-08 18:33:11 -0500

    Configured rate limiting to only allow low call volumes on the login endpoint, once a user is logged in the rate limit goes up to 1 call per second

--- src/main/kotlin/com/encryptix/configuration/RateLimitingConfiguration.kt
+++ src/main/kotlin/com/encryptix/configuration/RateLimitingConfiguration.kt
@@ -4,31 +4,54 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.application.ApplicationCallPipeline.ApplicationPhase.Plugins
 import io.ktor.server.plugins.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.time.Duration.Companion.minutes
 
-data class RateLimitConfig(var lastRequestTime: Long , var requestCount : Int )
+data class RateLimitConfig(var lastRequestTime: Long, var requestCount: Int)
 
-val rateLimitMap = ConcurrentHashMap<String,RateLimitConfig>()
+val rateLimitMap = ConcurrentHashMap<String, RateLimitConfig>()
 
 
-fun Application.configureRateLimiting(){
-    intercept(Plugins){
+fun Application.configureRateLimiting() {
+    intercept(Plugins) {
         val ip = call.request.origin.remoteHost
         val currentTime = System.currentTimeMillis()
         val rateLimitInfo = rateLimitMap.getOrPut(ip) { RateLimitConfig(currentTime, 0) }
 
-        if (currentTime - rateLimitInfo.lastRequestTime > 1.minutes.inWholeMilliseconds){
-            rateLimitInfo.requestCount = 1
-            rateLimitInfo.lastRequestTime = currentTime
-        }else{
-            rateLimitInfo.requestCount++
-            if (rateLimitInfo.requestCount > 5){
-                call.respond(HttpStatusCode.TooManyRequests, mapOf("Response" to "Too many requests, rate limit exceeded"))
-                finish()
+        if (call.request.uri == "/app/login") {
+            if (currentTime - rateLimitInfo.lastRequestTime > 1.minutes.inWholeMilliseconds) {
+                rateLimitInfo.requestCount = 1
+                rateLimitInfo.lastRequestTime = currentTime
+            } else {
+                rateLimitInfo.requestCount++
+                if (rateLimitInfo.requestCount > 6) {
+                    call.respond(
+                        HttpStatusCode.TooManyRequests,
+                        mapOf("Response" to "Too many requests, rate limit exceeded")
+                    )
+                    finish()
+
+                }
             }
+        } else {
+            if (currentTime - rateLimitInfo.lastRequestTime > 1.minutes.inWholeMilliseconds) {
+                rateLimitInfo.requestCount = 1
+                rateLimitInfo.lastRequestTime = currentTime
+            } else {
+                rateLimitInfo.requestCount++
+                if (rateLimitInfo.requestCount > 60) {
+                    call.respond(
+                        HttpStatusCode.TooManyRequests,
+                        mapOf("Response" to "Too many requests, rate limit exceeded")
+                    )
+                    finish()
+                }
+            }
+
         }
+
 
     }
 }
