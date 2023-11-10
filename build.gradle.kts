03551042e96083594e3b9198b33ccd9bcafeb082
Dustyn <dustyn_gibb@hotmail.com>
2023-11-09 19:31:19 -0500

    Upgraded ktor because the version it was using had some critical vulnerabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val postgres_version: String by project
 val h2_version: String by project
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.4"
+    id("io.ktor.plugin") version "2.3.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
 }
 
